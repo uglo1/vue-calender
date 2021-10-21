01e9899e99105e2809a1fc3e292b9edd6d5838da
uglo1 <yujiro.takahashi0912@gmail.com>
2021-10-21 16:11:15 +0900

    vuexの適用

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
     <h1>Calendar</h1>
-    <p>events:</p>
-    <p>{{ events }}</p>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
     <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    <CalendarDetails></CalendarDetails>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import { mapGetters, mapActions } from 'vuex';
+import CalendarDetails from './CalendarDetails.vue';
 
 export default {
   name: 'Calendar',
-  data: () => ({
-    events: [],
-  }),
+  components: {
+    CalendarDetails,
+  },
+  computed: {
+    ...mapGetters('events', ['events']),
+  },
   methods: {
-    fetchEvents() {
-      axios
-        .get('http://localhost:3000/events')
-        .then((response) => {
-          this.events = response.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
+    ...mapActions('events', ['fetchEvents']),
   },
 };
 </script>
