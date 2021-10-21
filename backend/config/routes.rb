cf0b951e78b7208bd8e767613a86c60fcef148dc
uglo1 <yujiro.takahashi0912@gmail.com>
2021-10-21 15:05:08 +0900

    変数名の修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  # get '/events', to: 'events#index'
-  # get '/events/:id', to: 'events#show'
-  # post '/events', to: 'events#create'
-  # put '/events/:id', to: 'events#update'
-  # delete '/events/:id', to: 'events#destroy'
-
-  resources :event, only: [:index, :show, :create, :update, :destroy]
+  resources :events, only: [:index, :show, :create, :update, :destroy]
 end
