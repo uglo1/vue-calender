60b27d928e79156776bfc2d1833a29c3737a9a45
uglo1 <yujiro.takahashi0912@gmail.com>
2021-10-21 22:07:17 +0900

    vuexストアに予定詳細ダイアログのアクションを追加した

--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -4,10 +4,10 @@ const apiUrl = 'http://localhost:3000';
 
 const state = {
   events: [],
+  event: null, // クリックされた予定
 };
 
 const getters = {
-  // events: (state) => state.events
   // 日付を文字列型から日付型へ変換する
   events: state => state.events.map(event => {
     return {
@@ -16,10 +16,18 @@ const getters = {
       end: new Date(event.end)
     };
   }),
+
+  // 上と同様に日付型に変換する
+  event: state => state.event ? {
+    ...state.event,
+    start: new Date(state.event.start),
+    end: new Date(state.event.end)
+  }: null,
 };
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  setEvent: (state, event) => (state.event = event),
 };
 
 const actions = {
@@ -27,6 +35,9 @@ const actions = {
     const response = await axios.get(`${apiUrl}/events`);
     commit('setEvents', response.data); // mutationの呼び出し
   },
+  setEvent({ commit }, event) {
+    commit('setEvent', event);
+  }
 };
 
 export default {
