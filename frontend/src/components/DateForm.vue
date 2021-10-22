7d1cac4a634f770259b9ff4c656ff25f360dd7af
uglo1 <yujiro.takahashi0912@gmail.com>
2021-10-22 18:21:12 +0900

    日付の開始、終了の選択の追加

---
+++ frontend/src/components/DateForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-menu offset-y>
+    <template v-slot:activator="{ on }">
+      <v-btn text v-on="on">{{ value || '日付を選択'}}</v-btn>
+    </template>
+
+    <v-date-picker
+      :value="value.replace(/\//g, '-')"
+      @input="$emit('input', $event.replace(/-/g, '/'))"
+      no-title
+    ></v-date-picker>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'DateForm',
+  props: ['value'],
+}
+</script>
--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -9,6 +9,10 @@
       <DialogSection icon="mdi-square" :color="event.color || 'blue'">
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
+      <DialogSection icon="mdi-clock-outline">
+        <DateForm v-model="startDate"></DateForm>
+        <DateForm v-model="endDate"></DateForm>
+      </DialogSection>
     </v-card-text>
     <v-card-action class="d-flex justify-end">
       <v-btn @click="submit">保存</v-btn>
@@ -19,17 +23,26 @@
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import DialogSection from './DialogSection.vue';
+import DateForm from './DateForm.vue';
+import { format } from 'date-fns';
 
 export default {
   name: 'EventFormDialog',
   components: {
     DialogSection,
+    DateForm,
   },
   data: () => ({
     name: '',
+    startDate: null,
+    endDate: null,
   }),
   computed: {
     ...mapGetters('events', ['event']),
+  },
+  created() {
+    this.startDate = format(this.event.start, 'yyyy/MM/dd');
+    this.endDate = format(this.event.end, 'yyyy/MM/dd');
   },
   methods: {
     ...mapActions('events', ['setEvent', 'setEditMode', 'createEvent']) ,
@@ -39,8 +52,8 @@ export default {
     submit() {
       const params = {
         name: this.name,
-        start: this.event.start,
-        end: this.event.end,
+        start: this.startDate,
+        end: this.endDate,
       };
       this.createEvent(params);
       this.closeDialog();
